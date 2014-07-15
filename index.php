f1a7a4087ab6fa736b35904b809994302388b48d
Matthew Zipkin <Matthew.Zipkin@gmail.com>
2014-07-14 17:54:18 -0700

    starting CW logon integration

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Easy Counterwallet</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Easy Counterwallet</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right" role="form">
+            <div class="form-group">
+              <input type="text" placeholder="Identifier" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Get Wallet</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Easy Counterwallet</h1>
+        <p>Enter your 12-word <a href="https://counterwallet.co/" target="_blank">Counterwallet</a> passphrase below.<br>This information will not leave your browser!</p>
+		  <div class="form-group col-sm-8">
+			<input type="password" class="form-control" id="CWpassphrase" placeholder="Enter your 12-word Counterwallet passphrase">
+		  </div>
+		  <button type="submit" onclick="acceptPP()" class="btn btn-default" id="acceptCWpassphrase">Accept</button>
+      </div>
+    </div>
+<? /*
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+*/ ?>
+      <hr>
+
+      <footer>
+        <p>Created by Matthew Zipkin 2014</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+	<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/ripemd160.js"></script>
+	<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js"></script>
+	<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/components/enc-base64-min.js"></script>
+    <script src="js/bitcore.js"></script>
+    <script src="js/mnemonic.js"></script>
+    <script src="js/ezxcp.js"></script>
+  </body>
+</html>
--- js/ezxcp.js
+++ js/ezxcp.js
@@ -1,3 +1,54 @@
+// counterwallet identifier
+function getCWID(passphrase){
+	var hashBase = CryptoJS.SHA256(passphrase);
+	var hash = CryptoJS.SHA256(hashBase).toString(CryptoJS.enc.Base64);
+	return hash;
+}
+
+// accept passphrase 
+function checkPP(passphrase){
+	var cleanPP = $.trim(passphrase.toLowerCase());
+	
+	var words = cleanPP.split(' ');
+    if (words.length != 12 && (words.length != 13 || words[0] != 'old')) {
+      return false;
+    }
+
+    if (words.length==13) {
+      words.shift();
+    }
+    
+    var valid = true;
+    words.forEach(function (word) {
+       if (Mnemonic.words.indexOf(word) == -1) {
+          valid = false;
+       }
+     });
+     if (!valid)
+     	return false;
+     else
+	     return cleanPP;
+}
+
+// click accept button
+function acceptPP(){
+	var PP = $('#CWpassphrase').val();
+	var cleanPP = checkPP(PP);
+	if(!cleanPP)
+		alert("Passphrase Error");
+	else
+		console.log(getCWID(cleanPP));
+}
+
+
+
+/*
+
+
+
+// load bitcore
+var bitcore = require('bitcore');
+
 // scrape master public key from logged in counterwallet
 var MASPUBKEY = WALLET.BITCOIN_WALLET.HierarchicalKey.derive('m/0\'/0').extendedPublicKeyString();
 
@@ -7,7 +58,7 @@ var NEWHDWALLET = new bitcore.HierarchialKey(MASPUBKEY);
 // generate addresses
 var HOWMANY = 3;
 var ADDRESSES = [];
-for (i = 0, i < HOWMANY; i++){
+for (i = 0; i < HOWMANY; i++){
 	var thisAddr = [];
 	thisAddr[0] = bitcore.Address.fromPubKey( SHIT.derive('m/' + i).eckey.public ).toString();
 	ADDRESSES.push(thisAddr);
@@ -34,8 +85,9 @@ function getJSON(url, success) {
 }
 
 // query blockscan API
-for (j = 0, j < ADDRESSES.length, j++){
+for (j = 0; j < ADDRESSES.length; j++){
   var bsAPi = 'http://blockscan.com/api2.aspx?module=balance&address=';
   var thisAddr = ADDRESSES[j][0];
   ADDRESSES[j][1] = getJSON(bsAPi + thisAddr, function(data){return data;});
 }
+*/
