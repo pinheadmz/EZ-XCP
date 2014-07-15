49764b3681101d8e7768948eb26354bf494b7a6b
Matthew Zipkin <Matthew.Zipkin@gmail.com>
2014-07-15 11:47:43 -0700

    working custom bit core modules

--- index.php
+++ index.php
@@ -1,60 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="favicon.ico">
 
-    <title>Easy Counterwallet</title>
+	<title>Easy Counterwallet</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Bootstrap core CSS -->
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
 
-  <body>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Easy Counterwallet</a>
+		</div>
+		<div class="navbar-collapse collapse">
+			<form class="navbar-form navbar-right" role="form">
+				<div class="form-group">
+					<input type="text" placeholder="Identifier" class="form-control">
+				</div>
+				<div class="form-group">
+					<input type="password" placeholder="Password" class="form-control">
+				</div>
+				<button type="submit" class="btn btn-success">Get Wallet</button>
+			</form>
+		</div><!--/.navbar-collapse -->
+	</div>
+</div>
 
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Easy Counterwallet</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right" role="form">
-            <div class="form-group">
-              <input type="text" placeholder="Identifier" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Get Wallet</button>
-          </form>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </div>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1>Easy Counterwallet</h1>
-        <p>Enter your 12-word <a href="https://counterwallet.co/" target="_blank">Counterwallet</a> passphrase below to create a watch-only wallet.<br>This information will not leave your browser!</p>
-		  <div class="form-group row">
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+	<div class="container">
+		<h1>Easy Counterwallet</h1>
+		<p>Enter your 12-word <a href="https://counterwallet.co/" target="_blank">Counterwallet</a> passphrase below to create a watch-only wallet.<br>This information will not leave your browser!</p>
+		<div class="form-group row">
 			<input type="password" class="form-control" id="CWpassphrase" placeholder="Enter your 12-word Counterwallet passphrase" value="accept rare smoke hook stain reply lip alas painful dirt tight flood">
-		  </div>
+		</div>
 		<div class="form-group row">
 			<div class="input-group">
 				<span class="input-group-addon">
@@ -64,57 +63,43 @@
 					<label for="showPP">Show passphrase</label>
 				</span>
 				<button type="submit" onclick="acceptPP()" class="btn btn-success" id="acceptCWpassphrase" style="float:right">Create watch-only wallet</button>
-			</div>
-			  
+			</div>			
 		</div>
-      </div>
-    </div>
+	</div>
+</div>
 
-    <div class="container">
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
+<div class="container">
+	<!-- Example row of columns -->
+	<div class="row" style="display:none" id="mainDisplay">
+		<div class="col-md-4">
+			<h2>Master Public Key:</h2>
+			<p><pre id="MasPubKeyDisplay"></pre></p>
+	 </div>
+		<div class="col-md-4">
+			<h2>Derived Addresses:</h2>
+			<p><pre id="addressesDisplay"></pre></p>
+			<p><a class="btn btn-default" onclick="loadMoreAddresses()" role="button">Load 3 more addresses...</a></p>
+		</div>
+	</div>
 
-      <hr>
+	<hr>
 
-      <footer>
-        <p>Created by Matthew Zipkin 2014</p>
-      </footer>
-    </div> <!-- /container -->
+	<footer>
+		<p>Created by Matthew Zipkin 2014</p>
+	</footer>
+</div> <!-- /container -->
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    
-<!--
-	<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js"></script>
-	<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/components/enc-base64-min.js"></script>
-	<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/aes.js"></script>
--->
-
-<!-- replace this with actual bitcore when available -->
-<script src="http://counterwallet.co/js/counterwallet-deps-min.js?v=01e1da406c05"></script>
-<script src="http://counterwallet.co/js/counterwallet-min.js?v=1260971d5021"></script>
-
-    <script src="js/mnemonic.js"></script>
-    <script src="js/ezxcp.js"></script>
-  </body>
+<!-- Bootstrap -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- Crypto -->
+<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha256.js"></script>
+<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/components/enc-base64-min.js"></script>
+<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/aes.js"></script>
+<!-- Bitcore, CW, and my code -->
+<script src="js/bitcore.js"></script>
+<script src="js/mnemonic.js"></script>
+<script src="js/ezxcp.js"></script>
+</body>
 </html>
--- js/ezxcp.js
+++ js/ezxcp.js
@@ -1,4 +1,5 @@
 // global
+var bitcore = require('bitcore');
 var WALLET = {};
 var PUBLIC_WALLET = {};
 
@@ -43,48 +44,54 @@ function acceptPP(){
 		genWallet(cleanPP);
 }
 
-// load wallet from master public key
-function loadWallet(masPubKey, numAddr){
-	var numAddr = (typeof numAddr !== 'undefined' ? numAddr : 3);
-		
-	// create new HD wallet form master public key
-	PUBLIC_WALLET.HK = new bitcore.HierarchicalKey(masPubKey);
+// update info display
+function refreshDisplay(){
+	$('#mainDisplay').slideDown();
+	$('#MasPubKeyDisplay').html(PUBLIC_WALLET.HK.extendedPublicKeyString());
+	$('#addressesDisplay').html('');
+	$.each(PUBLIC_WALLET.addresses, function(i,v){
+		$('#addressesDisplay').append(v.address + '\r\n');
+	});
+
+}
+
+// add derivative addresses to wallet
+function loadMoreAddresses(){
+	var currNum = PUBLIC_WALLET.addresses.length;
+	var numAddr = currNum + 3;
 	
 	// generate addresses
-	PUBLIC_WALLET.addresses = [];
-	for (var i = 0; i < numAddr; i++){
+	for (var i = currNum; i < numAddr; i++){
 		var thisAddr = {};
 		thisAddr.address = bitcore.Address.fromPubKey( PUBLIC_WALLET.HK.derive('m/' + i).eckey.public ).toString();
 		PUBLIC_WALLET.addresses.push(thisAddr);
 	}
+	
+	refreshDisplay();
+}
+
+// load wallet from master public key
+function loadWallet(masPubKey){
+	// create new HD wallet form master public key
+	PUBLIC_WALLET.HK = new bitcore.HierarchicalKey(masPubKey);
+	
+	// generate first four addresses
+	PUBLIC_WALLET.addresses = [];
+	loadMoreAddresses();
 /*
 	// get address information via BLOCKSCAN API
-	for (var i = 0; i < PUBLIC_WALLET.addresses.length; i++){	
-		$.ajax({
-			url: 'http://blockscan.com/api2.aspx?module=balance&address=' + PUBLIC_WALLET.addresses[i].address,
-			jsonp: "callback",
-			dataType: "jsonp",
-		 
-			// tell YQL what we want and that we want JSON
-			data: {
-				q: "select title,abstract,url from search.news where query=\"cat\"",
-				format: "json"
-			},
-		 
-			// work with the response
-			success: function( response ) {
-				console.log( response ); // server response
-			}
-		});
-		
-	
-	
-			PUBLIC_WALLET.addresses[i].info = $.getJSON('http://blockscan.com/api2.aspx?module=balance&address=' + PUBLIC_WALLET.addresses[i].address,
-														function(data){return data});
+	$.ajax({
+		url: 'http://blockscan.com/api2.aspx?module=balance&address=' + PUBLIC_WALLET.addresses[i].address,
+		jsonp: "callback",
+		dataType: "jsonp",
+	 
+		// work with the response
+		success: function( response ) {
+			console.log( response ); // server response
+		}
+	});
 */
-	}
-
-console.log(PUBLIC_WALLET);
+	refreshDisplay()
 }
 
 // generate wallet
@@ -100,59 +107,6 @@ function genWallet(cleanPP){
 	// get counterwallet base master public key - not the root
 	WALLET.masterPublicKey = WALLET.HK.derive('m/0\'/0').extendedPublicKeyString();
 	
-console.log(WALLET);
-	
 	// load new wallet from master public key
 	loadWallet(WALLET.masterPublicKey);
 }
-
-
-/*
-
-
-
-// load bitcore
-var bitcore = require('bitcore');
-
-// scrape master public key from logged in counterwallet
-var MASPUBKEY = WALLET.BITCOIN_WALLET.HierarchicalKey.derive('m/0\'/0').extendedPublicKeyString();
-
-// create new HD wallet form master public key
-var NEWHDWALLET = new bitcore.HierarchialKey(MASPUBKEY);
-
-// generate addresses
-var HOWMANY = 3;
-var ADDRESSES = [];
-for (i = 0; i < HOWMANY; i++){
-	var thisAddr = [];
-	thisAddr[0] = bitcore.Address.fromPubKey( SHIT.derive('m/' + i).eckey.public ).toString();
-	ADDRESSES.push(thisAddr);
-
-}
-
-// to get json data from url asyncronously
-// ref: http://stackoverflow.com/questions/2499567/how-to-make-a-json-call-to-a-url
-function getJSON(url, success) {
-
-    var ud = '_' + +new Date,
-        script = document.createElement('script'),
-        head = document.getElementsByTagName('head')[0] 
-               || document.documentElement;
-
-    window[ud] = function(data) {
-        head.removeChild(script);
-        success && success(data);
-    };
-
-    script.src = url.replace('callback=?', 'callback=' + ud);
-    head.appendChild(script);
-
-}
-
-// query blockscan API
-for (j = 0; j < ADDRESSES.length; j++){
-  var bsAPi = 'http://blockscan.com/api2.aspx?module=balance&address=';
-  var thisAddr = ADDRESSES[j][0];
-  ADDRESSES[j][1] = getJSON(bsAPi + thisAddr, function(data){return data;});
-}
-*/
