613dd1754ae26e16d7b320ad8ca9dde96ca15f56
Matthew Zipkin <Matthew.Zipkin@gmail.com>
2014-07-16 12:33:48 -0700

    bug fix

--- index.php
+++ index.php
@@ -22,32 +22,6 @@
 </head>
 
 <body>
-<? /*
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#">Easy Counterwallet</a>
-		</div>
-		<div class="navbar-collapse collapse">
-			<form class="navbar-form navbar-right" role="form">
-				<div class="form-group">
-					<input type="text" placeholder="Identifier" class="form-control">
-				</div>
-				<div class="form-group">
-					<input type="password" placeholder="Password" class="form-control">
-				</div>
-				<button type="submit" class="btn btn-success">Get Wallet</button>
-			</form>
-		</div><!--/.navbar-collapse -->
-	</div>
-</div>
-*/ ?>
 
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
